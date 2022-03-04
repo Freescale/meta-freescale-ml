Freescale/meta-freescale-ml

commit 24bbfd86fe6fd7bafce54bd2c75138fea99154fd

nn-imx: fix override syntax for variable
Signed-off-by: Chris Dimich <chris.dimich@boundarydevices.com>

recipes-libraries/nn-imx/nn-imx_1.2.0.bb
@@ -30,7 +30,7 @@ do_install () {
 
 # libneuralnetworks.so is dynamically loaded and thus needed in the
 # main package
-FILES:${PN}-dev_remove = "${libdir}/lib*.so"
+FILES:${PN}-dev:remove = "${libdir}/lib*.so"
 FILES:${PN} += "${libdir}/libneuralnetworks${SOLIBSDEV}"
 FILES:${PN}-dev += " \
     ${libdir}/libnnrt${SOLIBSDEV} \

commit 2c9f4a05b6d0eb8b59f18474f5d878c68563abb1

nn-imx: Fix wrong override syntax in INSANE_SKIP variable
Fixes: 24bbfd8 ("nn-imx: fix override syntax for variable")
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

recipes-libraries/nn-imx/nn-imx_1.2.0.bb
@@ -36,7 +36,7 @@ FILES:${PN}-dev += " \
     ${libdir}/libnnrt${SOLIBSDEV} \
     ${libdir}/libovxlib${SOLIBSDEV} \
 "
-INSANE_SKIP_${PN} += "dev-so"
+INSANE_SKIP:${PN} += "dev-so"
 
 # Works for i.MX 8 with GPU except for 8M Mini
 COMPATIBLE_MACHINE        = "(^$)"